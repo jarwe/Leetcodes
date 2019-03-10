8fe2025b631ce57e0c2c1eaed678097d569cdba1
Jerry YIN <jarwemailsky@126.com>
2019-03-10 22:31:39 +0800

    Correct its result by change get array of member to get its index.

--- src/main/java/cn/zilion/onetofifty/TwoSum.java
+++ src/main/java/cn/zilion/onetofifty/TwoSum.java
@@ -3,6 +3,18 @@ package cn.zilion.onetofifty;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
 public class TwoSum {
     /**
      * @Description: Two SUM problem, we can put all of members into Map at first
@@ -15,14 +27,14 @@ public class TwoSum {
      */
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
-        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-        for(int n : nums) {
-            map.put(n, true);
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
-                res[0] = nums[i];
-                res[1] = target - nums[i];
+                res[0] = i;
+                res[1] = map.get((target - nums[i]));
                 break;
             }
         }
